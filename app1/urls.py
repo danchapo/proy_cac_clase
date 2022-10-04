e0c6b524765535b85ded48b427d991977c18a2bb
Your Namedanchapo <danchapo@gmail.com>
2022-10-04 12:18:42 -0300

    actualizamos

--- app1/urls.py
+++ app1/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
+
 urlpatterns = [
-    path('hola_mundo',views.hola_mundo),
-    path('saludar',views.saludar, name="saludar_por_defecto"),
-    path('saludar/<str:nombre>',views.saludar, name="saludar"),
+    path('', views.index, name = 'inicio'),
+    path('quienessomos/', views.quienes_somos, name = 'quienes_somos'),
+    path('hola_mundo', views.hola_mundo),
+    path('saludar', views.saludar, name="saludar_por_defecto"),
+    path('saludar/<str:nombre>', views.saludar, name="saludar"),
+    path('proyectos/2022/07',views.ver_proyectos_2022_07),
     path('proyectos/<int:mes>/<int:anio>', views.ver_proyectos, name="ver_proyectos"),
+        # con el re_path vamos a especificar que el anio sea un caracter numérico (d) de entre 2 a 4 caracteres. 
+    re_path(r'^proyectos/(?P<anio>\d{2,4})', views.ver_proyectos_anio),
+    path('cursos/detalle/<slug:nombre_curso>', views.cursos_detalle, name= "curso_detalle"),
+        # la fc 're_path' es mas compleja: incluye metacaracteres (w,+) para definir cosas.
+    re_path(r'^cursos/(?P<nombre>\w+)/$', views.cursos, name="cursos"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,28 @@
 from django.http import HttpResponse
 # OJO que esto no va: from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
+
+def index(request):
+    if (request.method == 'GET'):
+        titulo = 'titulo cuado se accede con GET'
+    else:
+        titulo = f'titulo cuando se accede con otro metodo: {request.method}'
+    # lo siguiente muestra como obtener por GET los parametros que se puedan ingresar por url
+    parameters_get = request.GET.get('param1')
+    return HttpResponse(f"""
+        <h1>{titulo}</h1>
+        <p>valor de param1: {parameters_get}</p>
+        """)
+
+def quienes_somos(request):
+        # redirige a la url(path) con el name 'saludar_por_defecto' 
+    # return redirect('saludar_por_defecto')
+        # el redirect de abajo accede a la url con name 'saludar' y asigna 'Panchito' al str 'nombre'.
+    return redirect(reverse('saludar', kwargs={'nombre': 'Panchito'}))
+
 def hola_mundo(request):
     return HttpResponse('Hola Mundo Django')
 
@@ -12,8 +32,30 @@ def saludar(request, nombre='RANDOM'):
         <p>"probando cositas"</p>
         """)
 
-def ver_proyectos(request, mes, anio):
+def ver_proyectos(request, anio, mes = 1):
     return HttpResponse(f"""
-        <h1> proyectos del - {mes}/{anio}?</h1>
+        <h1> proyectos del - {mes}/{anio}</h1>
         <p>Listado de proyectos</p>
         """)
+
+def ver_proyectos_2022_07(request):
+    return HttpResponse(f"""
+        <h1>Proyectos del mes 7 del año 2022</h1>
+        <p>Listado de proyectos</p>
+    """) 
+
+def ver_proyectos_anio(request, anio):
+    return HttpResponse(f"""
+        <h1> proyectos del {anio}</h1>
+        <p>Listado de proyectos</p>
+        """)
+
+def cursos_detalle(request, nombre_curso):
+    return HttpResponse(f"""
+    <h1>{nombre_curso}</h1>
+    """)
+
+def cursos(request, nombre):
+    return HttpResponse(f"""
+    <h2>{nombre}</h2>
+    """)
